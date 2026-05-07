49281bf786403c6f6c1e81cdd991d8bf9f48c8b4
ravitejashivanadhuni <ravitejashivanadhuni@gmail.com>
2026-05-07 23:09:01 +0530

    SEO optimised

--- routes/sitemapRoutes.js
+++ routes/sitemapRoutes.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const { SitemapStream, streamToPromise } = require("sitemap");
 
 const Job = require("../models/job");
-const Walkin = require("../models/walkin");
+const Walkin = require("../models/Walkin");
 const Exam = require("../models/Exams");
 
 router.get("/sitemap.xml", async (req, res) => {
